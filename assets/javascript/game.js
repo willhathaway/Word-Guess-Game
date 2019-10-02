e0a1170aac0607920575c137573babcd3dfb424d
Will Hathaway <william.hathaway38@gmail.com>
2019-10-02 10:25:41 -0400

    start of javascript functionality

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+const genres = ['rock','pop','jazz','indie','hiphop','country','dance','folk','soul','metal','reggae','punk','funk']
+
+let mysteryWord = genres[Math.floor(Math.random() * genres.length)];
+
+document.log(mysteryWord);
+
+document.write("hello");
+
+//for (i = 0; i < genres.length; i ++) {
+
+//}
