bb0ca6f2c3536b1409663902d3c7fe4966e86a68
Will Hathaway <william.hathaway38@gmail.com>
2019-10-03 18:39:25 -0400

    added a CSS reset file, continued work on the displayWord function (stil incomplete)

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,29 +1,51 @@
+window.addEventListener('DOMContentLoaded', function(event) {
 
-//defining variables
+//defining the mystery words:
 
 const genres = ['rock','pop','jazz','indie','hiphop','country','dance','folk','soul','metal','reggae','punk','funk'];
 
+//defining the randomly chosen word:
+
 let mysteryWord = genres[Math.floor(Math.random() * genres.length)];
 
-let remainingGuesses = 15;
+//breaking the mystery word into an array of its individual letters:
 
-let mysteryArray = mysteryWord.split("");
+let mysteryArray = mysteryWord.split('');
+
+//defining a variable for a div with id='word' in the body of my HTML:
 
 let wordDiv = document.getElementById('word');
 
+//logging to console to confirm that the above variables work:
+
 console.log(mysteryWord);
-console.log(remainingGuesses);
 console.log(mysteryArray);
 
-function displayWord() {
+//function to loop through the array of the letters of the mystery word 
+//and display them in HTML as <p> paragraphs:
+
+document.onkeyup = function () {
+    
+function displayWord(mysteryArray, event) {
 
     for (let i = 0; i < mysteryArray.length; i++){
 
-        const p = document.createElement('p');
+        let userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
+        if (mysteryArray[i] = userGuess) {
+        let p = document.createElement('p');
         p.textContent = mysteryArray[i];
+        console.log(mysteryArray[i])
         wordDiv.appendChild(p);
+
+        }
 
     }
 }
 
 displayWord(mysteryArray);
+
+}
+
+    console.log('DOM fully loaded and parsed');
+});
